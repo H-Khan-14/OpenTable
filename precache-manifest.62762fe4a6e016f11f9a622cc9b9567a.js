efa675597a627f17cbaf6305e4fcc5a20ef595eb
H-Khan-14 <haseebkhan14@hotmail.com>
2021-09-09 17:35:34 -0400

    Updates

---
+++ precache-manifest.62762fe4a6e016f11f9a622cc9b9567a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "413b7804f19c62e746b0d5838dd17a7a",
+    "url": "/OpenTable/index.html"
+  },
+  {
+    "revision": "cad4258e9f18c71e7dce",
+    "url": "/OpenTable/static/css/main.af9d3aae.chunk.css"
+  },
+  {
+    "revision": "84e0b977ece1e2759f1a",
+    "url": "/OpenTable/static/js/2.1719af15.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/OpenTable/static/js/2.1719af15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cad4258e9f18c71e7dce",
+    "url": "/OpenTable/static/js/main.49b78427.chunk.js"
+  },
+  {
+    "revision": "9c48465c1e1ca5547f64",
+    "url": "/OpenTable/static/js/runtime-main.fdc6d7e6.js"
+  }
+]);
